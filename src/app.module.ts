8a7ea7ba1ba736e51254a541ccb0dedf0cfc8dd8
tirupathirao0201 <tirupathirao@divami.com>
2024-02-23 17:55:47 +0530

    fix:deleted app controller & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import "reflect-metadata"
 import { UserController } from './controllers/user/user.controller';
 import { UserService } from './services/user/user.service';
@@ -8,8 +6,8 @@ import { ChatGateway } from './websockets/chat/chat.gateway';
 import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [AuthModule],
-  controllers: [AppController,UserController],
-  providers: [AppService,UserService, ChatGateway],
+  controllers: [UserController],
+  providers: [UserService, ChatGateway],
 })
 export class AppModule {
 }
